8b1884889e3013432556ed2d4d933f123c39f8b2
damonweekendr <damon@weekendr.eu>
2021-09-11 16:15:02 +0200

    add error clause for login

--- components/Forms/Login/index.js
+++ components/Forms/Login/index.js
@@ -1,42 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
 import { signIn } from 'next-auth/client';
+import axios from 'axios';
 import cx from 'classnames';
 import Input from '../../Input';
 import InputButton from '../../Input/Button';
 import Button from '../../Button';
+import { Spinner } from 'react-bootstrap';
 
 function Login(props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoginError, setIsLoginError] = useState(false);
+  const [loginErrorMessage, setLoginErrorMessage] = useState('');
   const { register, handleSubmit } = useForm();
-  const handleLogin = (d) => {
-    console.log(d);
+  const handleLogin = async (d) => {
+    setIsLoading(true);
     const { email, password } = d;
-    signIn('credentials', {
-      email,
-      password,
-      // The page where you want to redirect to after a
-      // successful login
-      callbackUrl: `${window.location.origin}/profile`,
+    const body = {
+      user: {
+        password: password,
+        email: email,
+      },
+    };
+    const headers = {
+      'accept': '*/*',
+      'Content-Type': 'application/json',
+    };
+    const user = await axios.post('https://bk-mayn.herokuapp.com/api/user_sessions', body, {
+      headers,
     });
+    if (user.data.status === 1) {
+      setIsLoginError(false);
+      setLoginErrorMessage('');
+      signIn('credentials', {
+        email,
+        password,
+        callbackUrl: `${window.location.origin}/profile`,
+      });
+    } else {
+      setIsLoginError(true);
+      setLoginErrorMessage(user.data.message);
+    }
+    setIsLoading(false);
   };
 
   const onSubmit = (d) => handleLogin(d);
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="d-flex flex-column justify-content-between p-3"
-    >
-      <Input classnames="m-3" placeholder="Email" register={register} namespace="email" />
-      <Input
-        type="password"
-        classnames="m-3"
-        placeholder="Password"
-        register={register}
-        namespace="password"
-      />
-      <InputButton classnames="m-3" type="submit" value="Login" />
-    </form>
+    <React.Fragment>
+      {isLoading ? (
+        <div
+          className="d-flex align-items-center justify-content-center p-3"
+          style={{ minHeight: '264px' }}
+        >
+          <Spinner animation="grow" size="lg" />
+        </div>
+      ) : (
+        <React.Fragment>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="d-flex flex-column justify-content-between p-3"
+          >
+            <Input classnames="m-3" placeholder="Email" register={register} namespace="email" />
+            <Input
+              type="password"
+              classnames="m-3"
+              placeholder="Password"
+              register={register}
+              namespace="password"
+            />
+            {isLoginError && <p className="mx-3 p-0 red-1 sub-font">*{loginErrorMessage}</p>}
+            <InputButton classnames="mx-3 my-1" type="submit" value="Login" />
+          </form>
+        </React.Fragment>
+      )}
+    </React.Fragment>
   );
 }
 
